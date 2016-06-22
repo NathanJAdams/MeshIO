e63e3f4330acb45c6218282f4889d91df5d66d5d
Nathan <n8style@yahoo.com>
2016-06-22 14:45:32 +0100

    Removed duplicate error codes

--- src/ply/IPlySaver.java
+++ src/ply/IPlySaver.java
@@ -3,15 +3,6 @@ package ply;
 import java.util.List;
 
 public interface IPlySaver {
-   public static final int ERROR_FORMAT_NOT_FOUND                = 100;
-   public static final int ERROR_HEADER_NOT_FOUND                = 200;
-   public static final int ERROR_HEADER_NOT_RECOGNISED           = 201;
-   public static final int ERROR_HEADER_UNEXPECTED               = 202;
-   public static final int ERROR_HEADER_ELEMENT_COUNT_UNREADABLE = 210;
-   public static final int ERROR_DATA_NOT_FOUND                  = 300;
-   public static final int ERROR_DATA_INSUFFICIENT               = 301;
-   public static final int ERROR_DATA_NOT_PARSED                 = 302;
-
    void onSuccess();
 
    void onFailure(int errorCode);
