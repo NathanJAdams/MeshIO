7fa5bd7dd394fdb463574667318d9488f98b2d23
nadams <no@email.com>
2017-12-07 17:59:25 +0000

    Minor OBJ format improvements

--- src/com/ripplar_games/mesh_io/formats/obj/ObjFormat.java
+++ src/com/ripplar_games/mesh_io/formats/obj/ObjFormat.java
@@ -1,5 +1,10 @@
 package com.ripplar_games.mesh_io.formats.obj;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.regex.Pattern;
+
 import com.ripplar_games.mesh_io.IMeshBuilder;
 import com.ripplar_games.mesh_io.IMeshFormat;
 import com.ripplar_games.mesh_io.IMeshSaver;
@@ -7,30 +12,10 @@ import com.ripplar_games.mesh_io.MeshIOException;
 import com.ripplar_games.mesh_io.io.PrimitiveInputStream;
 import com.ripplar_games.mesh_io.mesh.IMesh;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.regex.Pattern;
-
 // TODO in progress
 public class ObjFormat implements IMeshFormat {
     private static final Pattern SPLITTER_PATTERN = Pattern.compile(" ");
 
-    private static <T extends IMesh> void appendVertex(IMeshBuilder<T> builder, String[] parts) {
-
-    }
-
-    private static <T extends IMesh> void appendTexCoord(IMeshBuilder<T> builder, String[] parts) {
-
-    }
-
-    private static <T extends IMesh> void appendNormal(IMeshBuilder<T> builder, String[] parts) {
-
-    }
-
-    private static <T extends IMesh> void appendFace(IMeshBuilder<T> builder, String[] parts) {
-
-    }
 
     @Override
     public String getFileExtension() {
@@ -51,27 +36,30 @@ public class ObjFormat implements IMeshFormat {
                 String firstPart = parts[0];
                 if ("#".equals(firstPart)) {
                     //comment
-                } else if ("f".equals(firstPart)) {
-                    appendFace(builder, parts);
                 } else {
-                    boolean isVertex = "v".equals(firstPart);
-                    boolean isTexCoord = !isVertex && "vt".equals(firstPart);
-                    boolean isNormal = !isVertex && !isTexCoord && "vn".equals(firstPart);
-                    if (isVertex || isTexCoord || isNormal) {
-                        currentVertexCount++;
-                        if (currentVertexCount > requestedVertexCount) {
-                            requestedVertexCount *= 2;
-                            builder.setVertexCount(requestedVertexCount);
-                        }
-                    }
-                    if (isVertex) {
-                        appendVertex(builder, parts);
-                    } else if (isTexCoord) {
-                        appendTexCoord(builder, parts);
-                    } else if (isNormal) {
-                        appendNormal(builder, parts);
+                    float[] floatArray = toFloatArrayIndex1(parts);
+                    if ("f".equals(firstPart)) {
+                        appendFace(builder, floatArray);
                     } else {
-                        throw new MeshIOException("Unrecognised element type: " + line + ". Expected a comment, \"v\", \"vt\", \"vn\" or \"f\"");
+                        boolean isVertex = "v".equals(firstPart);
+                        boolean isTexCoord = !isVertex && "vt".equals(firstPart);
+                        boolean isNormal = !isVertex && !isTexCoord && "vn".equals(firstPart);
+                        if (isVertex || isTexCoord || isNormal) {
+                            currentVertexCount++;
+                            if (currentVertexCount > requestedVertexCount) {
+                                requestedVertexCount *= 2;
+                                builder.setVertexCount(requestedVertexCount);
+                            }
+                        }
+                        if (isVertex) {
+                            appendVertex(builder, floatArray);
+                        } else if (isTexCoord) {
+                            appendTexCoord(builder, floatArray);
+                        } else if (isNormal) {
+                            appendNormal(builder, floatArray);
+                        } else {
+                            throw new MeshIOException("Unrecognised element type: " + line + ". Expected a comment, \"v\", \"vt\", \"vn\" or \"f\"");
+                        }
                     }
                 }
             }
@@ -84,5 +72,34 @@ public class ObjFormat implements IMeshFormat {
     @Override
     public void write(IMeshSaver saver, OutputStream os) throws MeshIOException {
         // TODO
+    }
+
+    private static float[] toFloatArrayIndex1(String[] parts) throws MeshIOException {
+        float[] floatArray = new float[parts.length - 1];
+        for (int i = 1; i < parts.length; i++) {
+            String part = parts[i];
+            try {
+                floatArray[i - 1] = Float.parseFloat(part);
+            } catch (NumberFormatException e) {
+                throw new MeshIOException("Could not parse value from: \"" + part + '\"');
+            }
+        }
+        return floatArray;
+    }
+
+    private static <T extends IMesh> void appendVertex(IMeshBuilder<T> builder, float[] parts) {
+        
+    }
+
+    private static <T extends IMesh> void appendTexCoord(IMeshBuilder<T> builder, float[] parts) {
+
+    }
+
+    private static <T extends IMesh> void appendNormal(IMeshBuilder<T> builder, float[] parts) {
+
+    }
+
+    private static <T extends IMesh> void appendFace(IMeshBuilder<T> builder, float[] parts) {
+
     }
 }
