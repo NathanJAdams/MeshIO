f257dbea29cc2dd5864616a61101f0b5d75bc0cd
Nathan <n8style@yahoo.com>
2018-04-02 14:36:20 +0100

    Simplified null objects

--- src/com/ripplargames/meshio/mesh/NullMesh.java
+++ src/com/ripplargames/meshio/mesh/NullMesh.java
@@ -4,11 +4,16 @@ import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.Set;
 
+import com.ripplargames.meshio.IMeshBuilder;
+import com.ripplargames.meshio.IMeshSaver;
 import com.ripplargames.meshio.vertex.VertexFormat;
+import com.ripplargames.meshio.vertex.VertexType;
 
-public class NullMesh implements IMesh {
-    private static final ByteBuffer INDICES_BUFFER = BufferUtil.with(new short[0]);
-    private static final ByteBuffer VERTICES_BUFFER = BufferUtil.with(new float[0]);
+public class NullMesh implements IMesh, IMeshBuilder<NullMesh>, IMeshSaver {
+    private static final int[] INDICES = new int[0];
+    private static final float[] VERTICES = new float[0];
+    private static final ByteBuffer INDICES_BUFFER = BufferUtil.with(INDICES);
+    private static final ByteBuffer VERTICES_BUFFER = BufferUtil.with(VERTICES);
 
     @Override
     public Set<VertexFormat> getVertexFormats() {
@@ -31,12 +36,47 @@ public class NullMesh implements IMesh {
     }
 
     @Override
-    public ByteBuffer getVertices(VertexFormat vertexFormat) {
+    public ByteBuffer getVertices(VertexFormat format) {
         return VERTICES_BUFFER;
     }
 
     @Override
     public ByteBuffer getIndices() {
         return INDICES_BUFFER;
+    }
+
+    @Override
+    public void clear() {
+    }
+
+    @Override
+    public void setVertexCount(int vertexCount) {
+    }
+
+    @Override
+    public void setFaceCount(int faceCount) {
+    }
+
+    @Override
+    public void setVertexDatum(int vertexIndex, VertexType vertexType, float vertexDatum) {
+    }
+
+    @Override
+    public void setFaceIndices(int faceIndex, int[] faceIndices) {
+    }
+
+    @Override
+    public NullMesh build() {
+        return this;
+    }
+
+    @Override
+    public float getVertexDatum(int vertexIndex, VertexType vertexType) {
+        return 0;
+    }
+
+    @Override
+    public int[] getFaceIndices(int faceIndex) {
+        return INDICES;
     }
 }
